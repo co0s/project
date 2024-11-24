ac715eb05fd4d15bfbe4b0faf25757736972cd24
co0s <irisdeliciya@gmail.com>
2024-11-25 02:13:41 +0800

    Delete JS_novice directory

--- JS_novice/11 StudentsList(server)/readme.txt
+++
@@ -1,3 +0,0 @@
-запустить файл backend в vsCode
-в терминале прописать node.index
-запустить фронтенд часть и live server
--- JS_novice/Homework3/task1(Password)/password.js
+++
@@ -1,7 +0,0 @@
-let password = '1123-_';
-
-if (password.length > 4 && (password.includes('-') || password.includes('_'))) {
-    console.log('Надежный пароль');
-} else {
-    console.log('Ненадежный пароль');
-}
